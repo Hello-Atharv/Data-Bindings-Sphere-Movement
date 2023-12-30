95c0c4366b15b34f52f412e2b33aa3eb9cf7fea1
Atharv Gupta <K035192@eltham-college.org.uk>
2023-12-30 14:06:51 +0000

    changedLists

---
+++ Data Bindings Sphere Movement/LinkedList.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBindingsSphereMovement
+{
+    class LinkedList<T>
+    {
+        private int length = 0;
+
+        private LinkedListNode<T> head = null;
+
+        public void Add(T item)
+        {
+            LinkedListNode<T> node = new LinkedListNode<T>(item);
+
+            if(head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                LinkedListNode<T> lastNode = FindAtIndex(length - 1);
+                lastNode.Next = node;
+                node.Previous = lastNode;
+            }
+
+            length++;
+        }
+
+        public T RemoveAtIndex(int index)
+        {
+            T item = default;
+
+            if(head != null)
+            {
+                LinkedListNode<T> node = FindAtIndex(index);
+                item = node.Data;
+                node.Previous.Next = node.Next;
+                node.Next.Previous = node.Previous;
+
+                length--;
+            }
+
+            return item;
+        }
+
+        public T[] AllData()
+        {
+            T[] data = new T[length];
+            LinkedListNode<T> current;
+
+            if (head != null)
+            {
+                current = head;
+                for (int i = 0; i < length; i++)
+                {
+                    data[i] = current.Data;
+                    current = current.Next;
+                }
+            }
+            else
+            {
+                data = null;
+            }
+
+            return data;
+        }
+
+        private LinkedListNode<T> FindAtIndex(int index)
+        {
+            LinkedListNode<T> node = null;
+            LinkedListNode<T> current = head;
+
+            if (index < length)
+            {
+                for(int i = 0;i< index; i++)
+                {
+                    current = current.Next;
+                }
+                node = current;
+            }
+
+            return node;
+        }
+
+        public T FindDataAtIndex(int index)
+        {
+            T data = default;
+            if(index < length)
+            {
+                data = FindAtIndex(index).Data;
+            }
+
+            return data;
+        }
+
+        public int Length
+        {
+            get { return length; }
+        }
+
+
+    }
+}
---
+++ Data Bindings Sphere Movement/LinkedListNode.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataBindingsSphereMovement
+{
+    class LinkedListNode<T>
+    {
+        private LinkedListNode<T> next = null;
+        private LinkedListNode<T> prev = null;
+        private T data;
+
+        public LinkedListNode(T data)
+        {
+            this.data = data;
+        }
+
+        public LinkedListNode<T> Next
+        {
+            get { return next; }
+            set { next = value; }
+        }
+
+        public LinkedListNode<T> Previous
+        {
+            get { return prev; }
+            set { prev = value; }
+        }
+
+        public T Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+    }
+}
